93ad558a473bd2016037c7d7fee40fea8d309b75
KNatali <natatsi24@gmail.com>
2022-06-05 19:31:56 +0200

    my comapnies vire

--- Joblinkt-Front/src/app/owner/my-companies/my-companies.component.ts
+++ Joblinkt-Front/src/app/owner/my-companies/my-companies.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { AddCommentComponent } from 'src/app/companies/add-comment/add-comment.component';
 import { EditCompanyInfoComponent } from 'src/app/companies/edit-company-info/edit-company-info.component';
+import { CompanyService } from 'src/app/core/services/company.service';
+import { JwtService } from 'src/app/core/services/jwt.service';
 import { AddCompanyComponent } from '../add-company/add-company.component';
 
 @Component({
@@ -11,15 +13,34 @@ import { AddCompanyComponent } from '../add-company/add-company.component';
 })
 export class MyCompaniesComponent implements OnInit {
   dilogRef: any;
-  constructor(public dialog: MatDialog) { }
+  comapnies:any[]=[];
+  myCompanies:any[]=[];
+  ownerId:any;
+  constructor(public dialog: MatDialog,
+    private companyService:CompanyService,
+    private jwtService:JwtService) { }
 
   ngOnInit(): void {
+    //this.ownerId=this.jwtService.getUserId();
   }
   openAddDialog(event: { stopPropagation: () => void; }) {
     this.dilogRef = this.dialog.open(AddCompanyComponent, {
       data: {
       }
     });
+  }
+
+  getMyCompanies(){
+    this.companyService.getAllCompaniesbyOwner(this.ownerId).subscribe(data=>{
+      this.comapnies=data;
+      this.comapnies.forEach((value: any, i: any) => {
+        if(value.isApproved){
+          this.myCompanies.push(value);
+        }
+      });
+    },error=>{
+      alert('Error! Try again!');
+    })
   }
 
 }
